9375fc5ae5973b74cbd03c8d91d62490b1546810
gyoszabi97 <45563373+gyoszabi97@users.noreply.github.com>
2019-04-16 12:29:38 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        InformatikusFrame informatikusFrame = new InformatikusFrame("Legyen Ön is Informatikus");
+    }
+}
---
+++ UjHallgatoDialog.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UjHallgatoDialog extends JDialog implements ActionListener {
+    private JTextField tf_nickname;
+
+    private JPanel panel_Panel;
+    private JPanel panel_inputPanel;
+    private JButton button_felviszGomb;
+
+    public UjHallgatoDialog(Frame szuloAblak) {
+        super(szuloAblak, "Új hallgató felvétele");
+
+        //GUI objektumok létrehozása
+
+        this.tf_nickname = new JTextField();
+        this.button_felviszGomb = new JButton("Felvisz");
+
+        //panelek és elrendezéseik létrehozása
+        this.panel_Panel = new JPanel();
+        this.panel_Panel.setLayout(new BorderLayout());
+        this.panel_inputPanel = new JPanel();
+        this.panel_inputPanel.setLayout(new GridLayout(1, 2));
+
+        //Főpanel dialogablakhoz adása
+        this.getContentPane().add(panel_Panel);
+
+        //input mezők
+        this.panel_inputPanel.add(new JLabel("Nickname"));
+        this.panel_inputPanel.add(this.tf_nickname);
+
+        //inputpanel a főpanelhez adása, középre
+
+        this.panel_Panel.add(panel_inputPanel, BorderLayout.CENTER);
+
+        //felviszgomb főpanel aljához adása
+
+        this.panel_Panel.add(button_felviszGomb, BorderLayout.SOUTH);
+
+        //Felvisz gomb eseményfigyelője
+        this.button_felviszGomb.addActionListener(this);
+
+        this.pack();
+
+    }
+        public void elokeszit(){
+            this.tf_nickname.setText("");
+        }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if(actionEvent.getSource().equals(this.button_felviszGomb)){
+            String nickname = this.tf_nickname.getText();
+
+            this.dispose();
+        }
+    }
+}
